36c1951f6d5e190dea604b22ae8b5ee5dbff5664
itslipfac <l33tw0rk@gmail.com>
2017-12-16 18:26:56 +0200

    #2_Finalized_command_parser
    - added getters for location and search string
    - updated readme
    - added external library to colorize console output

--- StringFinder/include/commandparser.h
+++ StringFinder/include/commandparser.h
@@ -1,6 +1,8 @@
 #ifndef COMMANDVALIDATOR_H
 #define COMMANDVALIDATOR_H
 
+#include <string>
+
 namespace {
 constexpr int ARGUMENTS_NUMBER = 3;
 constexpr int PATH_MIN_LENGTH = 0;
@@ -9,23 +11,41 @@ constexpr int STRING_MIN_LENGTH = 0;
 constexpr int STRING_MAX_LENGTH = 128;
 }
 
+// Class used to validate command line arguments and transform them in a
+// suitable format for processing
+// Provides getters for location and search string
 class CommandParser
 {
 public:
     CommandParser();
 
-    bool AreArgumentsValid(const int Argc, const char * const Argv[]) const;
+    // Validate location and search string
+    bool ValidateArguments(const int Argc, const char * const Argv[]);
 
+    // Used for debugging
     void PrintArguments(const int Argc, const char * const Argv[]) const;
 
+    std::string location() const;
+
+    std::string searchString() const;
+
 private:
+    // Validates that a string has the length between min and max limits
     bool HasValidLength(const char * const String, const int MinLength, const int MaxLength) const;
 
+    // Validates that the provided path is a file or a directory
     bool IsPathValid(const char * const Path) const;
 
+    // Validates the string length and content
     bool IsSearchStringValid(const char * const SearchString) const;
 
+    bool ContainsOnlyValidChars(const char * const SearchString) const;
+
+    // Show application usage
     void PrintHelp() const;
+
+    std::string _location;
+    std::string _searchString;
 };
 
 #endif // COMMANDVALIDATOR_H
--- StringFinder/src/commandparser.cpp
+++ StringFinder/src/commandparser.cpp
@@ -1,35 +1,38 @@
 #include "commandparser.h"
 
 #include <iostream>
-#include <string>
 #include <filesystem>
+#include <termcolor\termcolor.hpp>
 
 namespace fs = std::experimental::filesystem;
 
 using namespace std;
+using namespace termcolor;
 
-CommandParser::CommandParser()
+CommandParser::CommandParser() : _location{}, _searchString{}
 {
-
 }
 
-bool CommandParser::AreArgumentsValid(const int Argc, const char * const Argv[]) const
+bool CommandParser::ValidateArguments(const int Argc, const char * const Argv[])
 {
     bool areValid = false;
 
     if (Argc != ARGUMENTS_NUMBER)
     {
-        cout << "Invalid number of arguments: " << Argc << endl;
+        cout << red << "Invalid number of arguments: " << Argc << reset << endl;
 
         PrintHelp();
     }
-    else if (IsPathValid(Argv[1]) && IsSearchStringValid(Argv[2]))
+    else if ( IsPathValid(Argv[1]) && IsSearchStringValid(Argv[2]) )
     {
         areValid = true;
+
+        _location.assign(Argv[1]);
+        _searchString.assign(Argv[2]);
     }
     else
     {
-        cout << "Invalid arguments" << endl;
+        cout << red << "Invalid arguments" << reset << endl;
         PrintHelp();
     }
 
@@ -38,43 +41,53 @@ bool CommandParser::AreArgumentsValid(const int Argc, const char * const Argv[])
 
 void CommandParser::PrintArguments(const int Argc, const char * const Argv[]) const
 {
-    cout << "Number of arguments: " << Argc << endl;
+    cout << yellow << "Number of arguments: " << Argc << reset << endl;
 
     for (int i = 0; i < Argc; ++i)
     {
-        cout << "argv["  << i << "] = " << Argv[i] << endl;
+        cout << yellow << "argv["  << i << "] = " << Argv[i] << reset << endl;
     }
+}
+
+std::string CommandParser::location() const
+{
+    return _location;
+}
+
+std::string CommandParser::searchString() const
+{
+    return _searchString;
 }
 
 bool CommandParser::HasValidLength(const char * const String, const int MinLength, const int MaxLength) const
 {
-    const int stringLength = strlen(String);
-    bool      hasValidLength = ( (MinLength < stringLength) && (stringLength < MaxLength) );
+    const size_t stringLength = strlen(String);
+    const bool   hasValidLength = ( (MinLength < stringLength) && (stringLength < MaxLength) );
 
     return hasValidLength;
 }
 
 bool CommandParser::IsPathValid(const char * const Path) const
 {
-    bool      isValid = false;
-    const int pathLength = strlen(Path);
-    fs::path  path(Path);
+    bool           isValid = false;
+    const size_t   pathLength = strlen(Path);
+    const fs::path path(Path);
 
     if ( !HasValidLength(Path, PATH_MIN_LENGTH, PATH_MAX_LENGTH) )
     {
-        cout << "Invalid path: " << path << ". Length: " << pathLength << " is invalid." << endl;
+        cout << red << "Invalid path: " << path << ". Length: " << pathLength << " is invalid." << reset << endl;
     }
     else if ( !fs::exists(path) )
     {
-        cout << "Invalid path: " << Path << ". Location doesn't exit." << endl;
+        cout << red << "Invalid path: " << Path << ". Location doesn't exit." << reset << endl;
     }
     else if ( !fs::is_regular_file(path) && !fs::is_directory(path) )
     {
-        cout << "Invalid path: " << path << ". Location exists, but is neither a regular file nor a directory" << endl;
+        cout << red << "Invalid path: " << path << ". Location exists, but is neither a regular file nor a directory" << reset << endl;
     }
     else
     {
-        cout << "Path valid." << endl;
+        cout << green << "Path valid." << reset << endl;
 
         isValid = true;
     }
@@ -84,16 +97,20 @@ bool CommandParser::IsPathValid(const char * const Path) const
 
 bool CommandParser::IsSearchStringValid(const char * const SearchString) const
 {
-    bool      isValid = false;
-    const int stringLength = strlen(SearchString);
+    bool         isValid = false;
+    const size_t stringLength = strlen(SearchString);
 
     if ( !HasValidLength(SearchString, STRING_MIN_LENGTH, STRING_MAX_LENGTH) )
     {
-        cout << "Invalid search string: " << SearchString << ". Length: " << stringLength << " is invalid." << endl;
+        cout << red << "Invalid search string: " << SearchString << ". Length: " << stringLength << " is invalid." << reset << endl;
+    }
+    else if ( !ContainsOnlyValidChars(SearchString) )
+    {
+        cout << red << "Invalid search string: " << SearchString << ". It contains invalid characters." << reset << endl;
     }
     else
     {
-        cout << "Search string valid." << endl;
+        cout << green << "Search string valid." << reset << endl;
 
         isValid = true;
     }
@@ -101,9 +118,16 @@ bool CommandParser::IsSearchStringValid(const char * const SearchString) const
     return isValid;
 }
 
+bool CommandParser::ContainsOnlyValidChars(const char * const SearchString) const
+{
+    // TODO: add logic as per requirements
+
+    return true;
+}
+
 void CommandParser::PrintHelp() const
 {
-    cout << "Usage: ./StringFinder <path> <search_string>" << endl;
+    cout << yellow << "Usage: StringFinder.exe <path> <search_string>" << reset << endl;
 }
 
 
